49aff0a131891d507cf5c0d4b470bee8aa2f5746
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-04-26 20:45:38 +0100

    Temp change to ai model for testing - switch back after

--- api/views.py
+++ api/views.py
@@ -27,8 +27,7 @@ def ask_ai(request):
     try:
         client = Groq(api_key=api_key)
         completion = client.chat.completions.create(
-            # model='llama-3.3-70b-versatile',
-            model='llama-3.1-8b-instant',
+            model='llama-3.3-70b-versatile',
             max_tokens=4096,
             temperature=0.3,
             messages=[
